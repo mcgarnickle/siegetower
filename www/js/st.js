8f860744fae3ecce217419b6127d3e63ba129b09
mcgarnickle <mcbain10@hotmail.com>
2015-12-12 15:37:57 -0500

    dumbloop function

--- www/js/st.js
+++ www/js/st.js
@@ -1,14 +1,15 @@
-//dumb loop
-for (var i = 1; i < 50; i++) {
-  var note = i-100;
-  var time = i+1;
-  var velocity = i*10;
-  var dist = new Tone.Distortion(i*1000).toMaster();
-  var fm = new Tone.SimpleFM().connect(dist); 
+function dumbloop() {
+  for (var i = 1; i < 50; i++) {
+    var note = i-100;
+    var time = i+1;
+    var velocity = i*10;
+    var dist = new Tone.Distortion(i*1000).toMaster();
+    var fm = new Tone.SimpleFM().connect(dist); 
 
-  if (i % 2 == 0) {
-    fm.triggerAttack(note, time, velocity);
-  } else {
-    fm.triggerAttackRelease(note, time, velocity);
+    if (i % 2 == 0) {
+      fm.triggerAttack(note, time, velocity);
+    } else {
+      fm.triggerAttackRelease(note, time, velocity);
+    }
   }
 }
